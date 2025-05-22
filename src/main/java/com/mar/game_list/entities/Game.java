2fb214b6bd6a7de854310efc7b6432ae995d5170
Marcelo Cordeiro Basílio <marcelocbasilio@gmail.com>
2025-05-22 20:34:26 -0300

    Game seed.

--- src/main/java/com/mar/game_list/entities/Game.java
+++ src/main/java/com/mar/game_list/entities/Game.java
@@ -17,21 +17,26 @@ public class Game {
     private String platforms;
     private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
-    public Game() {}
+    public Game() {
+    }
 
     public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
-        this.id = id;
-        this.title = title;
-        this.year = year;
-        this.genre = genre;
-        this.platforms = platforms;
-        this.score = score;
-        this.imgUrl = imgUrl;
-        this.shortDescription = shortDescription;
-        this.longDescription = longDescription;
+        setId(id);
+        setTitle(title);
+        setYear(year);
+        setGenre(genre);
+        setPlatforms(platforms);
+        setScore(score);
+        setImgUrl(imgUrl);
+        setShortDescription(shortDescription);
+        setLongDescription(longDescription);
     }
 
     public Long getId() {
