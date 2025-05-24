551be9679a21aea06d0b22199a9258dbb2a96e0c
Marcelo Basílio <marcelocbasilio@gmail.com>
2025-05-24 17:11:18 -0300

    GET game by id.

--- src/main/java/com/mar/game_list/services/GameService.java
+++ src/main/java/com/mar/game_list/services/GameService.java
@@ -1,9 +1,11 @@
 package com.mar.game_list.services;
 
+import com.mar.game_list.dtos.GameDto;
 import com.mar.game_list.dtos.GameMinDto;
 import com.mar.game_list.entities.Game;
 import com.mar.game_list.repositories.GameRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,6 +18,13 @@ public class GameService {
         this.gameRepository = gameRepository;
     }
 
+    @Transactional(readOnly = true)
+    public GameDto findById(Long id) {
+        Game game = gameRepository.findById(id).get();
+        return new GameDto(game);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDto> findAll() {
         List<Game> games = gameRepository.findAll();
         return games.stream().map(GameMinDto::new).toList();
